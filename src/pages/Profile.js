339434a7946e05f8718829a6446ce1893c95db2b
unknown <everythingfree4200@gmail.com>
2025-08-31 09:23:46 +0530

    111

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>My Profile</h1>
+      <p>Welcome to your profile page!</p>
+      <p>You can add your details here.</p>
+    </div>
+  );
+};
+
+export default Profile;
