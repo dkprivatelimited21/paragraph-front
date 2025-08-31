e3dc41ec06cebef607cb4b104da9122d91e190b2
unknown <everythingfree4200@gmail.com>
2025-08-31 09:42:43 +0530

    0022

---
+++ src/pages/Notifications.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notifications = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Notifications</h1>
+      <p>You have no new notifications.</p>
+    </div>
+  );
+};
+
+export default Notifications;
